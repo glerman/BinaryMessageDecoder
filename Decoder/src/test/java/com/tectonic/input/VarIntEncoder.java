414e5101d1624475cc9b6aa0fbae7ca90b7c1009
glerman <glerman@outbrain.com>
2019-10-13 15:11:58 +0300

    Fixed VarIntEncoder util

--- Decoder/src/test/java/com/tectonic/input/VarIntEncoder.java
+++ Decoder/src/test/java/com/tectonic/input/VarIntEncoder.java
@@ -5,26 +5,27 @@ public class VarIntEncoder {
 
   public static byte[] encodeVarInt(final long x) {
     byte moreMask = (byte) 0b1000_0000;
-    byte noMoreMask = (byte) 0b0111_111;
-    byte valMask = (byte) 0b0111_111;
+    byte noMoreMask = (byte) 0b0111_1111;
+    byte valMask = (byte) 0b0111_1111;
     int bytesCount = requiredVarIntBytes(x);
     byte[] varInt = new byte[bytesCount];
 
     for (int i = 0; i < bytesCount; i++) {
-      byte b = (byte)(((byte) (x >> 7 * i)) & valMask);
-      byte modified = i < bytesCount - 1 ? (byte)(moreMask | b) : (byte)(noMoreMask | b);
+      byte b = (byte)(((byte) (x >> (7 * i))) & valMask);
+      byte modified = i < bytesCount - 1 ? (byte)(moreMask | b) : (byte)(noMoreMask & b);
       varInt[i] = modified;
     }
     return varInt;
   }
 
   static int requiredVarIntBytes(final long val) {
+    if (val == 0) return 1;
     int i = 0;
     for (; i < 64; i++) {
-      if (val >> i == 0) {
+      if (val >> i <= 0) {
         break;
       }
     }
-    return 1 + i/7;
+    return 1 + (i-1)/7;
   }
 }
--- Decoder/src/test/java/com/tectonic/input/VarIntEncoderTest.java
+++ Decoder/src/test/java/com/tectonic/input/VarIntEncoderTest.java
@@ -3,15 +3,12 @@ package com.tectonic.input;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class VarIntEncoderTest {
 
   @Test
   public void testRequiredVarIntBytes() throws Exception {
 
+    System.out.println((byte)0b0000_1000);
     Assert.assertEquals(1, VarIntEncoder.requiredVarIntBytes(0));
     Assert.assertEquals(1, VarIntEncoder.requiredVarIntBytes(0b0000_1000));
     Assert.assertEquals(2, VarIntEncoder.requiredVarIntBytes(0b1000_0100));
@@ -21,18 +18,10 @@ public class VarIntEncoderTest {
 
   @Test
   public void testEncodeDecode() throws Exception {
-
-    Assert.assertEquals(300, new MemoryScanner(VarIntEncoder.encodeVarInt(0b1010_1100_0000_0010)).decodeVarint(0).value);
+    Assert.assertEquals(8, new MemoryScanner(VarIntEncoder.encodeVarInt(8)).decodeVarint(0).value);
+    Assert.assertEquals(64, new MemoryScanner(VarIntEncoder.encodeVarInt(64)).decodeVarint(0).value);
+    Assert.assertEquals(300, new MemoryScanner(VarIntEncoder.encodeVarInt(300)).decodeVarint(0).value);
+    Assert.assertEquals(1378263, new MemoryScanner(VarIntEncoder.encodeVarInt(1378263)).decodeVarint(0).value);
   }
 
-//  @Test
-//  public void requiredVarIntBytes() {
-//    final long val = 300;
-//    List<Character> c = new ArrayList<>();
-//    for (int i = 0; i < 64; i++) {
-//      c.add(((byte)(val >> i) & 0B1) == 1 ? '1' : '0');
-//    }
-//    Collections.reverse(c);
-//    c.forEach(System.out::print);
-//  }
 }
