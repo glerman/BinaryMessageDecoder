90f3d4da06bc9262969ff89adfc9613e093153f0
glerman <glerman@outbrain.com>
2019-10-16 23:30:25 +0300

    added comments and asserts

--- Decoder/src/main/java/com/tectonic/input/MemoryScanner.java
+++ Decoder/src/main/java/com/tectonic/input/MemoryScanner.java
@@ -22,6 +22,7 @@ public class MemoryScanner {
 
   @VisibleForTesting
   VarInt decodeVarint(final int offset) {
+    //find varint bytes
     byte moreMask = (byte) 0B1000_0000;
     byte varIntValueMask = (byte) 0B0111_1111;
     List<Byte> varIntValueBytes = new ArrayList<>();
@@ -33,8 +34,9 @@ public class MemoryScanner {
       varIntValueBytes.add((byte)(currByte & varIntValueMask));
       currIdx++;
     }
-    assert varIntValueBytes.size() <= 8 : "A varint has 8 bytes at most";
+    assert varIntValueBytes.size() > 0 && varIntValueBytes.size() <= 8 : "A varint has between 0 and 8 bytes";
     assert currIdx == varIntValueBytes.size();
+    //extract number from bytes
     int value = 0;
     for (int i = 0; i < varIntValueBytes.size(); i++) {
       byte byteVal = varIntValueBytes.get(i);
@@ -106,6 +108,7 @@ public class MemoryScanner {
 
   private RawBlock scanBlock(final int offset) {
     VarInt blockLength = decodeVarint(offset);
+    assert blockLength.value >= 1 && blockLength.value <= data.length;
 
     if (blockLength.value == blockLength.length) { //empty block: no pointers or payload.
       assert blockLength.value == 1 : "Expecting the length of an empty block to be 1";
@@ -118,6 +121,8 @@ public class MemoryScanner {
     //scan pointers until we reach the zero byte, or the end of block (may not be a zero byte if there's no payload)
     while (scannedLength < blockLength.value) {
       VarInt pointer = decodeVarint(offset + scannedLength);
+      assert pointer.value < data.length;
+
       scannedLength += pointer.length;
       if (pointer.value == 0) { //optional zero byte reached
         assert pointer.length == 1;
@@ -125,10 +130,10 @@ public class MemoryScanner {
       }
       pointers.add(pointer);
     }
-    if (scannedLength == blockLength.value) { //block has pointers but no payload
+    if (scannedLength == blockLength.value) { //no payload
       return new RawBlock(offset, blockLength, pointers, null);
 
-    } else if (scannedLength < blockLength.value) { //block has payload and (maybe) pointers
+    } else if (scannedLength < blockLength.value) { //has payload
       return new RawBlock(offset, blockLength, pointers, scannedLength);
 
     } else {
